76d640dae1aaed8da64bb91d4809e94334df32f0
roby <roby@sahabatbisnis.co.id>
2024-02-12 09:02:17 +0700

    landing page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('../views/chat-ai/chat-ai.component').then(comp => comp.ChatAiComponent),
-  }
+    loadComponent: () => import('../views/home/home.component').then(comp => comp.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('../views/about/about.component').then(comp => comp.AboutComponent),
+  },
+  {
+    path: 'faq',
+    loadComponent: () => import('../views/faq/faq.component').then(comp => comp.FaqComponent),
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () => import('../views/contact/contact.component').then(comp => comp.ContactComponent),
+  },
 ];
